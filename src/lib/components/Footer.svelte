23622d3a817344968e60e9622065efe0502e2e81
Jakob Goldbach <jakob.goldbach@gmail.com>
2023-02-05 14:43:27 +0100

    base structure ready

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>Hello, I'm the footer.</footer>
+
+<style>
+    footer {
+        padding: 1rem;
+        background: lightskyblue;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
---
+++ src/routes/blog/post-two.md
@@ -0,0 +1,8 @@
+---
+title: Post Two
+date: "2022-12-14"
+---
+
+Hello, I am 2
+
+**Nice to meet you!**
